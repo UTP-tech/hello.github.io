c8d99f04fdd4ead048ed1a243d1c3e1b7d8c7122
UTP-tech <73649887+UTP-tech@users.noreply.github.com>
2020-10-29 18:56:34 +0530

    hello_project

---
+++ javascript/myscript.js
@@ -0,0 +1,11 @@
+// alert("This is javscript")
+// var rate= prompt("How much will you rate the code ")
+// alert("Tq for ur rate " +rate)
+// console.log(" Your are cool")
+
+var lbs =prompt("Weight in lbs?");
+
+var kg = lbs * 0.454;
+
+alert("That is " + kg+"Kilogram");
+console.log("Thanks you")
